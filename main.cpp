562468dd54c2a41a29e7efb56442bd68dad40c3f
Kyra Jaeger <87786202+jaegerkyra@users.noreply.github.com>
2021-10-21 13:03:41 -0500

    Update main.cpp
    Made changes so that user could keep petting Tony until 'x' is enter or number of pets does not exceed 7. Troubleshooting number of pets.

--- main.cpp
+++ main.cpp
@@ -1,38 +1,54 @@
+// PepporoniTony.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
 #include <iostream>
 using namespace std;
 
+//declare variables
+string Input;
+int numPets = 0;
+
+
+//Function for how Tony reacts to pets
+void petting() {
+
+    if (Input == "PET") {
+        if (numPets == 0) {
+            cout << "Tony liked that pet very much. He might like another pet." << endl;
+            cout << numPets << endl;
+        }
+        if (numPets < 4) {
+            cout << "Pepperoni Tony: Purrrrrrrrrrr!" << endl;
+            numPets++;
+            cout << numPets << endl;
+        }
+        else if (numPets < 7 && numPets >= 4) {
+            cout << "Pepperoni Tony is all petted out for the day. He doesn't want any more pets." << endl;
+            numPets++;
+            cout << numPets << endl;
+        }
+        else {
+            cout << "Pepperoni Tony has left the chat. Try coming back another time." << endl;
+            numPets++;
+            cout << numPets << endl;
+        }
+    }
+    else {
+        cout << "Pepperoni Tony: Merrooowwp?" << endl;
+        cout << numPets << endl;
+    }
+}
+
 int main()
 {
-    cout << "Hello! My name is Pepperoni Tony!\n" << "To pet me, enter 'PET'.\n";
-
-
-    string Input;
-    cin >> Input;
+    cout << "Hello! My name is Pepperoni Tony!\n" << "To pet me, enter 'PET'. To exit, enter 'x'. \n";
   
-  //Initialize the number of pets to 0
-    int numPets = 0;
 
-
-        if (Input == "PET") {
-          //if this is the first pet, encourage more pets
-            if (numPets == 0) {
-                cout << "Tony liked that pet very much. He might like another pet." << endl;
-                numPets++;
-            }
-          //as long is this not the sixth pet or more, Tony will purr
-            if (numPets < 6) {
-                cout << "Pepperoni Tony: Purrrrrrrrrrr!" << endl;
-                numPets++;
-            }
-          //After 5 pets, even Pepperoni Tony has had enough
-            else {
-                cout << "Pepperoni Tony is all petted out for the day. He doesn't want any more pets." << endl;
-            }
-        }
-        //If anything other than "PET" is entered, Tony will chirp back
-        else {
-            cout << "Pepperoni Tony: Merrooowwp?" << endl;
-        }
+    do {
+        cin >> Input;
+        petting();
+    } while ((Input != "x") || (numPets < 7));
     
     return 0;
 }
+
